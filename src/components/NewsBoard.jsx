616ec065d22a4c548a59e9bc47c7c405b89a5d9c
esther-v <esvalas@gmail.com>
2024-01-22 19:10:06 +0100

    add categories in navbar

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react"
 import NewsItem from "./NewsItem"
 
-const NewsBoard = () => {
+const NewsBoard = ({ category }) => {
 
   const [articles, setArticles] = useState([])
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
     fetch(url).then(response => response.json()).then(data => setArticles(data.articles))
-  }, [])
+  }, [category])
   return (
-    <div>
+    <div className="d-flex flex-column">
       <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-      {articles.map((article, index) => {
-        return <NewsItem 
-          key={index} 
-          title={article.title} 
-          description={article.description}
-          src={article.urlToImage}
-          url={article.url}
-        />
-      })}
+      <div className="margin-auto">
+        {articles.map((article, index) => {
+          return <NewsItem 
+            key={index} 
+            title={article.title} 
+            description={article.description}
+            src={article.urlToImage}
+            url={article.url}
+          />
+        })}
+      </div>
     </div>
   )
 }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,10 +1,10 @@
 const NewsItem = ({ title, description, src, url }) => {
   return (
-    <div className="card" style={{ maxWidth: "345px" }}>
-      <img src={src} className="card-img-top" alt="..." />
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{ maxWidth: "345px" }}>
+      <img src={src ? src : "https://t4.ftcdn.net/jpg/02/09/53/11/360_F_209531103_vL5MaF5fWcdpVcXk5yREBk3KMcXE0X7m.jpg"} style={{height: "200px", width: "325px"}} className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
+        <h5 className="card-title">{title.slice(0,50)}</h5>
+        <p className="card-text" style={{ height: '72px'}}>{description ? description.slice(0,90) : "news description non available...news description non available...news description non available..."}</p>
         <a href={url} className="btn btn-primary">Read more</a>
       </div>
     </div>
